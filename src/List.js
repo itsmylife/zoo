057b6cf03763c1d33c4215148f3fed622a5482ce
Ksenia Burgucheva <ksenia.burgucheva@gmail.com>
2024-09-24 11:24:19 +0200

    #1 feedback

--- src/List.js
+++ src/List.js
@@ -3,43 +3,47 @@ import { initialAnimals } from './data.js';
 import { getImageUrl } from './utils.js';
 
 export default function List() {
-  const [animals, setAnimals] = useState(
-    initialAnimals
-  );
-  return <>
-      <button onClick={() => {
-              setAnimals(initialAnimals.filter(a =>
-                a.type === "cute"
-              ));
-            }}>
-              All Cute Animals
-        </button>
-        <button onClick={() => {
-              setAnimals(initialAnimals.filter(a =>
-                a.type === "scary"
-              ));
-            }}>
-              All Scary Animals
-        </button>
-        <button onClick={() => {
-              setAnimals(initialAnimals);
-            }}>
-              Selectors Choice
-        </button>
-      <ul>{getList(animals)}</ul>
-    </>;
+  const [animals, setAnimals] = useState({ type: null, list: initialAnimals });
+
+  function handleClick(e) {
+    let type = e.target.id;
+
+    if (type && type !== "selector") {
+      setAnimals({
+        type: type,
+        list: initialAnimals.filter(a =>
+          a.type === type
+        )
+      });
+    } else {
+      setAnimals({ type: type, list: initialAnimals });
+    }
+  }
+
+  return (<>
+    <button className="filter-button" id="cute" onClick={handleClick}>
+      All Cute Animals
+    </button>
+    <button className="filter-button" id="scary" onClick={handleClick}>
+      All Scary Animals
+    </button>
+    <button className="filter-button" id="selector" onClick={handleClick}>
+      Selectors Choice
+    </button>
+    <ul>{getList(animals.list)}</ul>
+  </>);
 }
 
-function getList (animals) {
+function getList(animals) {
   return animals.map(animal =>
-    <li key={"animal-" + animal.id}>
-      <img
+    <li className="animal-item" key={"animal-" + animal.id}>
+      <img className="animal-picture"
         src={getImageUrl(animal)}
         alt={animal.name}
       />
-      <p>
+      <p className="animal-description">
         <b>{animal.name}</b><br />
-          {animal.description}
+        {animal.description}
       </p>
     </li>
   );
